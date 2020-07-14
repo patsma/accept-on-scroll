f00a31e0e51f82611c9c2a308b2e9ad0edde5aec
patryk-smakosz <patryksmakosz1@gmail.com>
2020-07-14 16:46:29 +0200

    added observer, readme and comments

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Enable button on scroll - ES6, simple
+
+Visit  https://enable-button-on-scroll.netlify.app
--- scroll-to-accept.js
+++ scroll-to-accept.js
@@ -1,3 +1,18 @@
-console.log('works')
-
 const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+        //    stop the observe on the button
+        ob.unobserve(terms.lastElementChild)
+    }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+});
+
+ob.observe(terms.lastElementChild)
